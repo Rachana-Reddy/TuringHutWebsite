af901c8ff212fd30878efd2e467dabf6315d6375
Rachana <bh.rachana11@gmail.com>
2018-07-29 01:10:14 +0530

    Add Navbar
    Signed-off-by: Rachana <bh.rachana11@gmail.com>

--- js/main.js
+++ js/main.js
@@ -2,8 +2,23 @@
 
     'use-strict';
 
+    $('#hamburger').click(function() {
+        $(this).toggleClass('open');
+    });
+
     $(window).on('load', function () {
         $('.preloader').fadeOut(3000);
+    });
+
+    $(window).on('scroll', function(event) {
+        var scrollValue = $(window).scrollTop();
+        if (scrollValue > 20) {
+            $('.navbar').addClass('affix');
+            $('.navbar').removeClass('non-affix');
+        } else {
+            $('.navbar').removeClass('affix');
+            $('.navbar').addClass('non-affix');
+        }
     });
 
 })(jQuery);
