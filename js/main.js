b7e5bc18cad07c3aa0a1f2a8adf9a67ee56eb2e2
Rachana <bh.rachana11@gmail.com>
2018-08-04 20:27:24 +0530

    Add about us section
    Signed-off-by: Rachana <bh.rachana11@gmail.com>

--- js/main.js
+++ js/main.js
@@ -20,5 +20,31 @@
             $('.navbar').addClass('non-affix');
         }
     });
+       var statSection = $(".stats"),
+        stats = $(".stats_count");
+    console.log(statSection);
+    statSection.waypoint({
+        handler: function(direction) {
+            if (direction === "down") {
+                stats.each(function() {
+                    var $this = $(this);
+                    $({
+                        Counter: 0
+                    }).animate({
+                        Counter: $this.text()
+                    }, {
+                        duration: 3000,
+                        easing: 'swing',
+                        step: function(curValue) {
+                            $this.text(Math.ceil(curValue));
+                        }
+                    });
+                });
+            }
+            // trigger once only
+            this.destroy();
+        },
+        offset: "90%"
+    });
 
 })(jQuery);
